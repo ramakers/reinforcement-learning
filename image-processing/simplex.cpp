58ddcd33682e80c8f9606610ae515a76838aa689
ramakers <j.ramakers@hotmail.de>
2016-03-29 00:14:38 +0200

    image lib added

---
+++ image-processing/simplex.cpp
@@ -0,0 +1,25 @@
+#include</usr/include/boost/gil/gil_all.hpp>
+
+using namespace boost::gil;
+
+void x_gradient(const gray8c_view_t& src,
+                const gray8s_view_t& dst)
+{
+  for(int y=1;y<src.height()-1; ++y){
+    gray8c_view_t::x_iterator src1_it = src.row_begin(y-1);
+    gray8s_view_t::x_iterator src2_it = dst.row_begin(y+1);
+    gray8s_view_t::x_iterator dst_it = dst.row_begin(y);
+    
+    for(int x=0;x<src.width(); ++x){
+      *dst_it =((*src2_it) - (*src1_it))/ 2;
+      ++dst_it; ++src1_it; ++src2_it;
+    }
+  
+  }
+}
+
+int main(){
+  
+  return 0;
+}
+
