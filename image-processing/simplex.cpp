51900af3a4605feea3185431e5eac4ec2dd6b462
ramakers <j.ramakers@hotmail.de>
2016-03-30 18:39:48 +0200

    locator added

--- image-processing/simplex.cpp
+++ image-processing/simplex.cpp
@@ -18,6 +18,23 @@ void x_gradient(const gray8c_view_t& src,
   }
 }
 
+void x_gradient(const gray8c_view_t& src,
+                const gray8s_view_t& dst)
+{
+  gray8c_view_t::xy_locator src_loc = src.xy_at(0,1);
+  
+  for(int y=1;y<src.height()-1; ++y){
+    gray8s_view_t::x_iterator dst_it = dst.row_begin(y);
+    
+    for(int x=0;x<src.width(); ++x){
+      *dst_it =(src_loc(0,1) - src_loc(0,-1))/ 2;
+      ++dst_it;
+      ++src_loc.x();
+    }
+  }
+}
+
+
 int main(){
   
   return 0;
